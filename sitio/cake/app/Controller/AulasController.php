9192522ca17158c69426c0cc7e821d74b8cf267f
Daniel <daobmo@gmail.com>
2014-08-24 16:02:23 -0600

    corregido problema de índices en aulas para administrador

--- sitio/cake/app/Controller/AulasController.php
+++ sitio/cake/app/Controller/AulasController.php
@@ -6,6 +6,10 @@ class AulasController extends AppController {
 	public $uses = array('Curso');
 
     public function index($id) {
+    	if($_SESSION['tipo_usuario'] == 1) {	
+    		$_SESSION['id_curso'] = $id;
+		}
+		
 		$this->set('curso', $this->Curso->find('first', array('conditions' => array('Curso.id' => $_SESSION['id_curso']))));
 		
         if(isset($_SESSION['tipo_usuario']) and $_SESSION['tipo_usuario'] <= 3 ) {
