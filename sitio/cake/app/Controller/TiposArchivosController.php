0c93901dc6901c51ceb3a18b42a2cf23cbcbd18d
Malavasi <f.adrian59@yahoo.com>
2014-11-02 19:36:43 -0600

    extensiones terminado

--- sitio/cake/app/Controller/TiposArchivosController.php
+++ sitio/cake/app/Controller/TiposArchivosController.php
@@ -31,7 +31,7 @@ class TiposArchivosController extends AppController {
         if(isset($_SESSION['tipo_usuario']) and $_SESSION['tipo_usuario']==1 )
         {
            
-            if ($this->Curso->delete($id)) {
+            if ($this->Programas->delete($id)) {
                 $this->Session->setFlash(
                     __('La extension ha sido eliminada.', h($id))
                 );
@@ -44,6 +44,27 @@ class TiposArchivosController extends AppController {
             $this->redirect(array('controller' =>'inicio','action' => 'index'));     
         }
     }
+
+     public function add() {
+    	
+		
+        if(isset($_SESSION['tipo_usuario']) and $_SESSION['tipo_usuario']==1 )
+        {
+            if ($this->request->is('post')) {
+                $this->Programas->create();
+                if ($this->Programas->save($this->request->data)) {
+                    $this->Session->setFlash(__('Nueva extension creado.'));
+                    return $this->redirect(array('action' => 'index'));
+                }
+                $this->Session->setFlash(__('La extensión no se ha podido crear. Por favor, intente de nuevo.'));
+            }
+        }
+        else
+        {
+                $this->redirect(array('controller' =>'inicio','action' => 'index'));                
+        }
+    }
+
 
 }
 ?>
